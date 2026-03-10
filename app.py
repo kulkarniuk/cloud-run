543ed6ee38a06ffb377e78c9d79f23017fbd066e
UdayKK <uk.sun@yahoo.com>
2026-03-10 17:25:55 +0530

    uk commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Cloud Run"
+    return "Hello Cloud Run , this is my first deployment on Google Cloud Run using GitHub Actions!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
