3ba08ae19e994216be855c02cfc96a0dd553e113
UdayKK <uk.sun@yahoo.com>
2026-03-10 19:26:49 +0530

    testuk

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud Run!"
+    return "Hello from Cloud Run"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
